e629164aac645dd9c930eb2aa517f2aa897eb302
nazran <nazran91@gmail.com>
2024-11-07 19:12:17 +0600

    provide source target

--- mapstruct/src/main/java/com/nazran/mapstruct/mapper/InterestMenuMapper.java
+++ mapstruct/src/main/java/com/nazran/mapstruct/mapper/InterestMenuMapper.java
@@ -11,9 +11,9 @@ public interface InterestMenuMapper {
 
     InterestMenuMapper INSTANCE = Mappers.getMapper(InterestMenuMapper.class);
 
-//    @Mapping(target = "interestMenuId", source = "interestMenuId")
-//    @Mapping(target = "interestMenuName", source = "interestMenuName")
-//    @Mapping(target = "interestMenuCode", source = "interestMenuCode")
+    @Mapping(target = "interestMenuId", source = "interestMenuId")
+    @Mapping(target = "interestMenuName", source = "interestMenuName")
+    @Mapping(target = "interestMenuCode", source = "interestMenuCode")
     InterestMenuDto toDto(InterestMenu interestMenu);
 
     InterestMenu toEntity(InterestMenuDto interestMenuDto);
