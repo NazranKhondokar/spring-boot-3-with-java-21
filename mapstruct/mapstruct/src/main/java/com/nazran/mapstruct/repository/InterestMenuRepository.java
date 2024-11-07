7cbd5134f116c17aa38758b0c42f250396554d92
nazran <nazran91@gmail.com>
2024-11-07 19:03:05 +0600

    update directory

--- mapstruct/mapstruct/src/main/java/com/nazran/mapstruct/repository/InterestMenuRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nazran.mapstruct.repository;
-
-import com.nazran.mapstruct.entity.InterestMenu;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InterestMenuRepository extends JpaRepository<InterestMenu, Long> {
-}
