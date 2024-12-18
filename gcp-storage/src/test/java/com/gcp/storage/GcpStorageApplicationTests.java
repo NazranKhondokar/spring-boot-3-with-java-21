49e6bdb8ab07cea980ba1ca3d190d4fd14fcddc3
nazran <nazran@gmail.com>
2024-12-18 16:59:02 +0600

    init commit

---
+++ gcp-storage/src/test/java/com/gcp/storage/GcpStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcp.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
